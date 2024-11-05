161e87b7ccb66a3cccac361608562a9363633529
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-11-05 10:58:18 -0500

    fgfg

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -1,5 +1,6 @@
 using API_1.Data;
 using API_1.Interfaces;
+using API_1.Interfaces.Dtos.Stock;
 using API_1.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,9 +13,57 @@ public class StockRepository : IStockRepository
     {
         _context = context;
     }
+
+    public async Task<Stock> CreateAsync(Stock stockModel)
+    {
+        await _context.Stocks.AddAsync(stockModel);
+        await _context.SaveChangesAsync();
+        return stockModel;
+    }
+
+    public async Task<Stock?> DeleteAsync(int id)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+        if (stockModel == null)
+        {
+            return null;
+        }
+        _context.Stocks.Remove(stockModel);
+        await _context.SaveChangesAsync();
+        return stockModel;
+    }
+
     public Task<List<Stock>> GetAllAsync()
     {
         return _context.Stocks.ToListAsync();
     }
 
+    public async Task<Stock?> GetByIdAsync(int id)
+    {
+       return await _context.Stocks.FindAsync(id);
+    }
+
+    public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+    {
+        var existingStock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (existingStock == null)
+        {
+            return null;
+
+        }
+        existingStock.Symbol = stockDto.Symbol;
+        existingStock.CompanyName = stockDto.CompanyName;
+        existingStock.Purchase = stockDto.Purchase;
+        existingStock.LastDiv = stockDto.LastDiv;
+        existingStock.Industry = stockDto.Industry;
+        existingStock.MarketCap = stockDto.MarketCap;
+
+        await _context.SaveChangesAsync();
+
+        return existingStock;
+
+
+    }
 }
